8b13e9bf0fec2d2658da7d957cfa55ac61405bf7
Emanueljuarez <zerauleuname@gmail.com>
2022-03-04 10:49:34 -0600

    Repaso

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -21,7 +21,7 @@ export default function Header() {
 
   return (
 	<>
-		<header>
+		{/* <header>
 			<figure>
 				LOGO
 			</figure>
@@ -43,6 +43,7 @@ export default function Header() {
 								</li>
 								<li>{currentUser.name}</li>
 							</>
+							
 						) :
 						(
 							<>
@@ -71,10 +72,45 @@ export default function Header() {
 				</ul>
 
 			</nav>
-		</header>
-	
+		</header> */}
+		<header className="bg-indigo-600">
+  <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+    
+	{
+		authStatus ?
+		(
+			<>
+			<div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+      
+				<div className="ml-10 space-x-4">
+					<Link to="/"
+						onClick={() => {logoutUser()}}  className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">Cerrar Sesión</Link>
+					<a  className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50">{currentUser.name}</a>
+				</div>
+			</div>
+			</>
+		):
+		(
+			<>
+			<div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+      
+				<div className="ml-10 space-x-4">
+					<Link to="/registro"
+						 className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">Crear cuenta</Link>
+					<Link to="/login" href="#" className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50">Iniciar sesión</Link>
+				</div>
+			</div>
+			</>
+		)
+	}
+  </nav>
+</header>
 	</>
-
+	
 
   )
 }
+
+
+
+
